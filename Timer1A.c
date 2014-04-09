ec15cdc00a9b6bf5b4ef032a1a5cb7c4b986c941
annyan <ann.yanxuebin@gmail.com>
2014-04-08 23:46:39 -0500

    first_draft
    working on priority donation_annyan

---
+++ Timer1A.c
@@ -0,0 +1,166 @@
+// Timer0A.c
+// Runs on LM4F120
+// Use Timer0A in periodic mode to request interrupts at a particular
+// period.
+// Daniel Valvano
+// May 13, 2013
+
+/* This example accompanies the book
+   "Embedded Systems: Introduction to ARM Cortex M Microcontrollers"
+   ISBN: 978-1469998749, Jonathan Valvano, copyright (c) 2013
+   Volume 1, Program 9.8
+
+  "Embedded Systems: Real Time Interfacing to ARM Cortex M Microcontrollers",
+   ISBN: 978-1463590154, Jonathan Valvano, copyright (c) 2013
+   Volume 2, Program 7.5, example 7.6
+
+ Copyright 2013 by Jonathan W. Valvano, valvano@mail.utexas.edu
+    You may use, edit, run or distribute this file
+    as long as the above copyright notice remains
+ THIS SOFTWARE IS PROVIDED "AS IS".  NO WARRANTIES, WHETHER EXPRESS, IMPLIED
+ OR STATUTORY, INCLUDING, BUT NOT LIMITED TO, IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE APPLY TO THIS SOFTWARE.
+ VALVANO SHALL NOT, IN ANY CIRCUMSTANCES, BE LIABLE FOR SPECIAL, INCIDENTAL,
+ OR CONSEQUENTIAL DAMAGES, FOR ANY REASON WHATSOEVER.
+ For more information about my classes, my research, and my books, see
+ http://users.ece.utexas.edu/~valvano/
+ */
+#include "UART2.h"
+#include "tm4c123gh6pm.h"
+
+#define NVIC_EN0_INT21          0x00200000  // Interrupt 21 enable
+/*#define NVIC_EN0_R              (*((volatile unsigned long *)0xE000E100))  // IRQ 0 to 31 Set Enable Register
+#define NVIC_PRI5_R             (*((volatile unsigned long *)0xE000E414))  // IRQ 13 to 15 Priority Register
+#define TIMER1_CFG_R            (*((volatile unsigned long *)0x40031000))//annyan
+#define TIMER1_TAMR_R           (*((volatile unsigned long *)0x40031004))
+#define TIMER1_CTL_R            (*((volatile unsigned long *)0x4003100C))
+#define TIMER1_IMR_R            (*((volatile unsigned long *)0x40031018))
+#define TIMER1_MIS_R            (*((volatile unsigned long *)0x40031020))
+#define TIMER1_ICR_R            (*((volatile unsigned long *)0x40031024))
+#define TIMER1_TAILR_R          (*((volatile unsigned long *)0x40031028))
+#define TIMER1_TAPR_R           (*((volatile unsigned long *)0x40031038))
+#define TIMER1_TAR_R            (*((volatile unsigned long *)0x40031048))//end of annyan
+#define TIMER1_TAV_R						(*((volatile unsigned long *)0x40031050))*/
+#define TIMER_CFG_16_BIT        0x00000004  // 16-bit timer configuration,
+                                            // function is controlled by bits
+                                            // 1:0 of GPTMTAMR and GPTMTBMR
+#define TIMER_TAMR_TACDIR       0x00000010  // GPTM Timer A Count Direction
+#define TIMER_TAMR_TAMR_PERIOD  0x00000002  // Periodic Timer mode
+#define TIMER_CTL_TAEN          0x00000001  // GPTM TimerA Enable
+#define TIMER_IMR_TATOIM        0x00000001  // GPTM TimerA Time-Out Interrupt
+                                            // Mask
+#define TIMER_ICR_TATOCINT      0x00000001  // GPTM TimerA Time-Out Raw
+                                            // Interrupt
+#define TIMER_TAILR_TAILRL_M    0x0000FFFF  // GPTM TimerA Interval Load
+                                            // Register Low
+//#define SYSCTL_RCGC1_R          (*((volatile unsigned long *)0x400FE104))
+#define SYSCTL_RCGC1_TIMER1     0x00020000  // timer 0 Clock Gating Control
+
+//modified by annyan and Zirui to drive a pin to high or low
+/*#define GPIO_PORTD_DATA_R       (*((volatile unsigned long *)0x400073FC))
+#define GPIO_PORTD_DIR_R        (*((volatile unsigned long *)0x40007400))
+#define GPIO_PORTD_AFSEL_R      (*((volatile unsigned long *)0x40007420))
+#define GPIO_PORTD_DEN_R        (*((volatile unsigned long *)0x4000751C))
+#define GPIO_PORTD_AMSEL_R      (*((volatile unsigned long *)0x40007528))
+#define GPIO_PORTD_PCTL_R       (*((volatile unsigned long *)0x4000752C))
+#define SYSCTL_RCGC2_R          (*((volatile unsigned long *)0x400FE108))*/
+#define SYSCTL_RCGC2_GPIOD      0x00000008  // port D Clock Gating Control
+//#define PIND0                    (*((volatile unsigned long *)0x40007004))
+//end of modification
+
+void DisableInterrupts(void); // Disable interrupts
+void EnableInterrupts(void);  // Enable interrupts
+long StartCritical (void);    // previous I bit, disable interrupts
+void EndCritical(long sr);    // restore I bit to previous value
+void WaitForInterrupt(void);  // low power mode
+//Modified by annyan lab3
+void (*PeriodicTask1)(void);   // user function
+void (*PeriodicTask2)(void);   // user function
+
+//Modified by annyan and Zirui to drive a pin to high or low
+//void GPIO_Init(void){  volatile unsigned long delay;
+//  SYSCTL_RCGC2_R |= SYSCTL_RCGC2_GPIOD; // 1) activate port D
+//  delay = SYSCTL_RCGC2_R;           // allow time for clock to stabilize
+//                                    // 2) no need to unlock PD3-0
+//  GPIO_PORTD_AMSEL_R &= ~0x0F;      // 3) disable analog functionality on PD3-0
+//  GPIO_PORTD_PCTL_R &= ~0x0000FFFF; // 4) GPIO
+//  GPIO_PORTD_DIR_R |= 0x0F;         // 5) make PD3-0 out
+//  GPIO_PORTD_AFSEL_R &= ~0x0F;      // 6) regular port function 
+//  GPIO_PORTD_DEN_R |= 0x0F;         // 7) enable digital I/O on PD3-0
+//}
+//End of Modification
+// ***************** Timer0A_Init ****************
+// Activate Timer0A interrupts to run user task periodically
+// Inputs:  task is a pointer to a user function
+//          period in usec
+// Outputs: none
+unsigned int count=0;
+void OS_AddPeriodicThread(void(*task)(void), 
+   unsigned long period, unsigned long priority){
+	long sr;
+	//GPIO_Init();//for the PIN0 output
+	sr = StartCritical(); 
+		//UART_OutChar(count+'0');
+		//SYSCTL_RCGC1_R |= SYSCTL_RCGC1_TIMER1+0x00080000; // 0) activate timer0
+		TIMER1_CTL_R &= ~0x00000001;     // 1) disable timer0A during setup
+		TIMER1_CFG_R = 0;
+		TIMER1_CFG_R = 0x00000000;       // 2) configure for 32-bit timer mode
+		TIMER1_TAMR_R = 0x00000012;      // 3) configure for periodic mode, default down-up settings
+		TIMER1_TAILR_R = period-1;       // 4) reload value
+		TIMER1_ICR_R = 0x00000001;       // 6) clear timer1A timeout flag
+		TIMER1_IMR_R |= 0x00000001;      // 7) arm timeout interrupt
+		NVIC_PRI5_R = (NVIC_PRI5_R&0xFFFF1FFF)|(priority<<13); // 8) priority 2
+		NVIC_EN0_R = NVIC_EN0_INT21;     // 9) enable interrupt 21 in NVIC
+		TIMER1_CTL_R |= 0x00000001;      // 10) enable timer0A
+		PeriodicTask1 = task;
+	EndCritical(sr);
+}
+	 void OS_AddPeriodicThread2(void(*task)(void), 
+   unsigned long period, unsigned long priority){
+	long sr;
+	//GPIO_Init();//for the PIN0 output
+	sr = StartCritical(); 
+	
+		//SYSCTL_RCGC1_R |= 0x00080000; // 0) activate timer0
+		TIMER3_CTL_R &= ~0x00000001;     // 1) disable timer0A during setup
+		TIMER3_CFG_R = 0;
+		TIMER3_CFG_R = 0x00000000;       // 2) configure for 32-bit timer mode
+		TIMER3_TAMR_R = 0x00000012;      // 3) configure for periodic mode, default down-up settings
+		TIMER3_TAILR_R = period-1;       // 4) reload value
+		//TIMER1_TAPR_R = 49;              // 5) 1us timer1A, not functional in 32-bit mode
+		TIMER3_ICR_R = 0x00000001;       // 6) clear timer1A timeout flag
+		TIMER3_IMR_R |= 0x00000001;      // 7) arm timeout interrupt
+		NVIC_PRI8_R = (NVIC_PRI8_R&0x1FFFFFFF)|(priority<<29); // 8) priority 2
+		NVIC_EN1_R |= 8;     // 9) enable interrupt 35 in NVIC
+		TIMER3_CTL_R |= 0x00000001;      // 10) enable timer0A
+		PeriodicTask2 = task;
+	EndCritical(sr);
+}
+
+//modified by annyan lab3
+void Timer1A_Handler(void){
+//  PIND0=1;
+	long status;
+//	status= StartCritical();
+	TIMER1_ICR_R = TIMER_ICR_TATOCINT;// acknowledge timer1A timeout
+	status= StartCritical();
+	(*PeriodicTask1)();                // execute user task
+  
+	
+	EndCritical(status);
+	//UART_OutChar('a');
+
+}
+
+void Timer3A_Handler(void){
+//  PIND0=1;
+	long status;
+
+	TIMER3_ICR_R = TIMER_ICR_TATOCINT;// acknowledge timer1A timeout
+	status= StartCritical();
+	(*PeriodicTask2)();                // execute user task
+  
+	
+	EndCritical(status);
+ //UART_OutChar('b');
+}
---
+++ os.c
@@ -0,0 +1,631 @@
+//Modified by Annyan and Zirui
+#include "PLL.h"
+#include "os.h"
+#include "UART2.h"
+#include <stddef.h>
+#include <stdlib.h>
+#include "tm4c123gh6pm.h"
+
+void DisableInterrupts(void); // Disable interrupts
+void EnableInterrupts(void);  // Enable interrupts
+long StartCritical (void);    // previous I bit, disable interrupts
+void EndCritical(long sr);    // restore I bit to previous value
+void WaitForInterrupt(void);  // low power mode
+void PendSV(void);
+
+
+/***************************************Lab2_Part1*****************************************/
+/***************************************SYS_TICK****************************************/
+#define NVIC_ST_CTRL_R          (*((volatile unsigned long *)0xE000E010))
+#define NVIC_ST_RELOAD_R        (*((volatile unsigned long *)0xE000E014))
+#define NVIC_ST_CURRENT_R       (*((volatile unsigned long *)0xE000E018))
+#define NVIC_SYS_PRI3_R         (*((volatile unsigned long *)0xE000ED20))
+#define NVIC_INT_CTRL_R         (*((volatile unsigned long *)0xE000ED04))
+#define PEND0                   (*((volatile unsigned long *)0xE000E200))  
+#define NVIC_ST_CTRL_COUNT      0x00010000  // Count flag
+#define NVIC_ST_CTRL_CLK_SRC    0x00000004  // Clock Source
+#define NVIC_ST_CTRL_INTEN      0x00000002  // Interrupt enable
+#define NVIC_ST_CTRL_ENABLE     0x00000001  // Counter mode
+#define NVIC_ST_RELOAD_M        0x00FFFFFF  // Counter load value
+#define PIND0 (*((volatile unsigned long *)0x40007004))
+/*******************************TCB**************************************************/
+#define THREADS_NUM 10
+#define STACKSIZE 100
+#define DEAD 0xff78
+#define ACTIVE 0x09dd
+#define BLOCKED 0xff12
+#define INVALID_PRIORITY 18
+
+TCB_Type TCBS [THREADS_NUM];
+TCB_Type *RunPt;
+TCB_Type *NextPt;
+TCB_Type *DeadPt;
+long Stack [THREADS_NUM][STACKSIZE];
+
+void StartOS(void);
+void OS_DisableInterrupts(void);
+int count_timer2 = 0;
+void SetInitialStack (int i){
+	TCBS [i].sp = & Stack [i][STACKSIZE - 16]; //should this warning be a problem???
+	Stack [i][STACKSIZE-1] = 0x01000000;//Thumb bit
+	Stack [i][STACKSIZE-3] = 0x14141414;//R14
+	Stack [i][STACKSIZE-4] = 0x12121212;//R12
+	Stack [i][STACKSIZE-5] = 0x03030303;//R3
+	Stack [i][STACKSIZE-6] = 0x02020202;//R2
+	Stack [i][STACKSIZE-7] = 0x01010101;//R1
+	Stack [i][STACKSIZE-8] = 0x00000000;//R0
+	Stack [i][STACKSIZE-9] = 0x11111111;//R11
+	Stack [i][STACKSIZE-10] = 0x10101010;//R10
+	Stack [i][STACKSIZE-11] = 0x09090909;//R9
+	Stack [i][STACKSIZE-12] = 0x08080808;//R8
+	Stack [i][STACKSIZE-13] = 0x07070707;//R7
+	Stack [i][STACKSIZE-14] = 0x06060606;//R6
+	Stack [i][STACKSIZE-15] = 0x05050505;//R5
+	Stack [i][STACKSIZE-16] = 0x04040404;//R4
+
+}
+
+void OS_Init(){
+	RunPt=&TCBS[0];
+	DeadPt = NULL;
+	OS_DisableInterrupts();
+	//PLL_Init(); //set processor clock to 50MHZ
+	NVIC_ST_CTRL_R = 0;
+	NVIC_ST_CURRENT_R = 0;
+	NVIC_SYS_PRI3_R =((NVIC_SYS_PRI3_R & 0x00ffffff)|(6<<29));//priority 7;
+	NVIC_SYS_PRI3_R = (NVIC_SYS_PRI3_R & 0xFF1FFFFF)|(7<<21);
+	Timer2A_Init();
+}
+/*********required to be written?********************/
+
+void OS_Launch(unsigned long theTimeSlice){
+	NVIC_ST_RELOAD_R = theTimeSlice-1;
+	NVIC_ST_CTRL_R = 0x00000007;//enable, core clock and interrupt arm
+	StartOS();
+
+}
+void OS_Suspend (void){
+	NVIC_ST_CURRENT_R = 0;
+	NVIC_INT_CTRL_R = 0x04000000;//trigger systick_handler
+}
+//variable stackSize unused
+int thread_num =0;
+int thread_pt=0;
+unsigned int count_die=0;
+unsigned int count_num = 0;
+int OS_AddThread (void(*task)(void), unsigned long stackSize, unsigned long priority){
+
+	long status;
+	status = StartCritical();
+	
+	
+	
+	/*for(i=0; i< thread_pt; i++){
+		if (TCBS[i].status == DEAD){
+			SetInitialStack (i);
+			TCBS[i].Id = thread_num+1;
+		  TCBS[i].priority = priority;
+	    TCBS[i].sleep = 0;//Initialization
+	    TCBS[i].blocked = 0;//Initialization
+	    TCBS[i].status = ACTIVE;
+			Stack[i][STACKSIZE-2] = (long)task;
+			++thread_num;
+			EndCritical(status);
+			return 1;
+		}
+			
+	}*/
+	/*if (thread_num-count_die >=10||((count_num >=10) &&(!DeadPt))){
+		EndCritical(status);
+		return 0;}*/
+	if (DeadPt){
+		SetInitialStack (DeadPt->Id-1);
+		DeadPt->priority = priority;
+		DeadPt->sleep = 0;
+		DeadPt->status = ACTIVE;
+		//Modified by annyan for priority donation
+		DeadPt->is_donated=0;
+		DeadPt->priority_temp=INVALID_PRIORITY;
+		//End of modification
+		Stack[DeadPt->Id - 1][STACKSIZE-2] = (long)task;
+		DeadPt = NULL;
+
+		//++count_alive;
+		EndCritical(status);
+		return 1;
+	}
+	SetInitialStack (thread_num);
+	
+	if(thread_num){
+		TCBS[thread_num-1].Next = &TCBS[thread_num];
+	}
+	TCBS[thread_num].Next= &TCBS[0];
+	TCBS[thread_num].Id = thread_num+1;//0 is reserved for special usage
+	TCBS[thread_num].priority = priority;
+	TCBS[thread_num].sleep = 0;//Initialization
+	TCBS[thread_num].status = ACTIVE;
+	TCBS[thread_num].priority_temp=INVALID_PRIORITY;
+	TCBS[thread_num].is_donated=0;
+	Stack[thread_num][STACKSIZE-2] = (long)task; //PC
+	++thread_num;
+//	++thread_pt;
+	++count_num;
+	//++count_alive;
+	
+	EndCritical(status);
+	return 1;
+}
+//End of Modification
+
+
+/*********************************Modified by Zirui**************************************/
+
+unsigned long OS_TimeDifference(unsigned long start, unsigned long stop){
+	//if (start<stop) return (stop-start);
+	//else return start-stop;
+	return stop-start;
+	//return 0;
+}
+unsigned long OS_Time(void){
+	return TIMER2_TAV_R + count_timer2*80000;//NVIC_ST_CURRENT_R;//TIMER2_TAR_R;//
+	//return 0;
+}
+
+
+#define JITTERSIZE 64
+#define JTTERAMOUNT 4
+unsigned long JitterHistogram[JTTERAMOUNT][JITTERSIZE]={0,};
+long MaxJitter[JTTERAMOUNT];   
+unsigned static long lastTime[JTTERAMOUNT]; 
+unsigned long thisTime[JTTERAMOUNT];
+
+void jitterSetThisTime(char jitterNum){
+	thisTime[jitterNum] = OS_Time();
+}
+
+
+void jitterSetLastTime(char jitterNum){
+	lastTime[jitterNum] = thisTime[jitterNum];
+}
+
+long jitterCalc(char jitterNum, unsigned int period){
+	long jitter; 
+	unsigned long diff = OS_TimeDifference(lastTime[jitterNum],thisTime[jitterNum]);
+	if(diff>period){
+		jitter = (diff-period+4)/8;  // in 0.1 usec
+	}else{
+		jitter = (period-diff+4)/8;  // in 0.1 usec
+	}
+	if(jitter > MaxJitter[jitterNum]){
+		MaxJitter[jitterNum] = jitter; // in usec
+	}       // jitter should be 0
+	if(jitter >= JITTERSIZE){
+		jitter = JITTERSIZE-1;
+	}
+	JitterHistogram[jitterNum][jitter]++; 
+	return jitter;
+}
+
+long maxJitter(char jitterNum){
+	return MaxJitter[jitterNum];
+}
+
+long jitterHistogram(long jitter, char jitterNum){
+	if (jitter<JITTERSIZE) return JitterHistogram[jitterNum][jitter];
+	else return JitterHistogram[jitterNum][JITTERSIZE-1];
+}
+
+/********************************End of Modification*************************************/
+
+
+//Modified by annyan
+struct Sema4 BoxFree, DataValid;
+unsigned long static MailBox; // zw
+unsigned long OS_MailBox_Recv(void){
+	static unsigned long data=0;
+	OS_bWait (&DataValid);
+	data = MailBox;
+	OS_bSignal (&BoxFree);
+	return data;
+}
+void OS_MailBox_Send(unsigned long data){
+	OS_bWait (&BoxFree);
+	MailBox = data;
+	OS_bSignal (&DataValid);
+	return;
+}
+//end of modification
+
+// ******** OS_MailBox_Init ************
+// Initialize communication channel
+// Inputs:  none
+// Outputs: none
+//Modified by annyan
+/*Initialize bsema4 DataValid, 1 stands for there is data
+ *Boxfree, 1 stands for mailbox is free
+ */
+void OS_MailBox_Init(void){
+	
+	OS_InitSemaphore(&BoxFree, 1);//Initially BoxFree is 1 stands for an empty mailbox, 
+	                              //Data Valid is 0 stands for no data
+	OS_InitSemaphore(&DataValid, 0);
+}
+//end of modification
+//Modified by annyan and Zirui
+//unsigned long fifo;
+#define FIFOSIZE 128
+long volatile *fifoPutPt;
+long volatile *fifoGetPt;
+long static Fifo[FIFOSIZE];
+struct Sema4 DataRoomLeft, mutex, DataAvailable;
+
+void OS_Fifo_Init(unsigned long size){
+	OS_InitSemaphore(&DataRoomLeft, FIFOSIZE);//size later
+	OS_InitSemaphore(&DataAvailable, 0);
+	OS_InitSemaphore(&mutex, 1);
+	fifoGetPt=fifoPutPt=&Fifo[0];
+}
+
+long OS_Fifo_Size(void){
+	return DataAvailable.Value;
+}
+unsigned long OS_Fifo_Get(void){
+	unsigned long dataPt;
+	OS_Wait (&DataAvailable);
+	OS_bWait (&mutex);
+	dataPt=*fifoGetPt;
+	if (fifoGetPt==&Fifo[FIFOSIZE-1]) fifoGetPt=&Fifo[0]; //wrap
+	else fifoGetPt++;
+	OS_bSignal (&mutex);
+	OS_Signal (&DataRoomLeft);
+	return dataPt;
+}
+
+int OS_Fifo_Put(unsigned long data){
+	int flag = 0;//0 for failed, 1 for successful
+	volatile long *nextPt;
+	//UART_OutChar('f');
+	OS_Wait (&DataRoomLeft);
+	OS_bWait (&mutex);
+	//UART_OutChar('U');
+	nextPt=++fifoPutPt;
+	if (nextPt==&Fifo[FIFOSIZE]) nextPt=&Fifo[0]; //wrap
+	*fifoPutPt=data;
+	fifoPutPt=nextPt;
+	flag = 1;
+	OS_bSignal (&mutex);
+	OS_Signal (&DataAvailable);
+	return flag;
+}
+
+//end of modification
+// ******** OS_Kill ************
+// kill the currently running thread, release its TCB and stack
+// input:  none
+// output: none
+void OS_Kill(void){
+	//Assert (not in context switch)
+	long status;
+	status = StartCritical();
+	count_die++;
+	DeadPt = RunPt;
+	RunPt->status = DEAD;
+	EndCritical(status);
+	OS_Suspend();//kill itself immediately
+	
+}
+
+void OS_Sleep(unsigned long sleepTime){
+	/*************Take it out and put into sleeplist*****************/
+	long status;
+	status = StartCritical();
+	RunPt->sleep = sleepTime;
+	EndCritical(status);
+  OS_Suspend();
+}
+
+//Modified by annyan
+void PortF4_Init(void){ 
+                                // (a) activate clock for port F
+  SYSCTL_RCGC2_R |= SYSCTL_RCGC2_GPIOF;
+  GPIO_PORTF_DIR_R &= ~0x10;    // (c) make PF4 in (built-in button)
+  GPIO_PORTF_AFSEL_R &= ~0x10;  //     disable alt funct on PF4
+  GPIO_PORTF_DEN_R |= 0x10;     //     enable digital I/O on PF4
+                                //     configure PF4 as GPIO
+  GPIO_PORTF_PCTL_R = (GPIO_PORTF_PCTL_R&0xFFF0FFFF)+0x00000000;
+  GPIO_PORTF_AMSEL_R = 0;       //     disable analog functionality on PF
+  GPIO_PORTF_PUR_R |= 0x10;     //     enable weak pull-up on PF4
+  GPIO_PORTF_IS_R &= ~0x10;     // (d) PF4 is edge-sensitive
+  GPIO_PORTF_IBE_R &= ~0x10;    //     PF4 is not both edges
+  GPIO_PORTF_IEV_R &= ~0x10;    //     PF4 falling edge event
+  GPIO_PORTF_ICR_R = 0x10;      // (e) clear flag4
+  GPIO_PORTF_IM_R |= 0x10;      // (f) arm interrupt on PF4
+  NVIC_PRI7_R = (NVIC_PRI7_R&0xFF00FFFF)|0x00A00000; // (g) priority 5
+  NVIC_EN0_R |=(1<<30);  // (h) enable interrupt 30 in NVIC
+  //EnableInterrupts();           // (i) Program 5.3
+	/*********************end of modification***************************************/
+}
+
+void PortF0_Init(void){ 
+   unsigned delay;                             // (a) activate clock for port F
+  SYSCTL_RCGC2_R |= SYSCTL_RCGC2_GPIOF;
+	 delay = SYSCTL_RCGC0_R;         // 8) allow time for clock to stabilize
+
+  delay = SYSCTL_RCGC0_R;         // 8) allow time for clock to stabilize
+  GPIO_PORTF_LOCK_R
+= 0x4C4F434B; // unlock GPIO Port F 
+	GPIO_PORTF_CR_R
+= 0x1F; // allow changes to
+	
+  GPIO_PORTF_DIR_R &= ~0x01;    // (c) make PF0 in (built-in button)
+  GPIO_PORTF_AFSEL_R &= ~0x01;  //     disable alt funct on PF0
+  GPIO_PORTF_DEN_R |= 0x01;     //     enable digital I/O on PF0
+                                //     configure PF4 as GPIO
+  GPIO_PORTF_PCTL_R = (GPIO_PORTF_PCTL_R&0xFFFFFFF0)+0x00000000;
+  GPIO_PORTF_AMSEL_R = 0;       //     disable analog functionality on PF
+  GPIO_PORTF_PUR_R |= 0x01;     //     enable weak pull-up on PF0
+  GPIO_PORTF_IS_R &= ~0x01;     // (d) PF0 is edge-sensitive
+  GPIO_PORTF_IBE_R &= ~0x01;    //     PF0 is not both edges
+  GPIO_PORTF_IEV_R &= ~0x01;    //     PF0 falling edge event
+  GPIO_PORTF_ICR_R = 0x01;      // (e) clear flag0
+  GPIO_PORTF_IM_R |= 0x01;      // (f) arm interrupt on PF0
+  NVIC_PRI7_R = (NVIC_PRI7_R&0xFF00FFFF)|0x00A00000; // (g) priority 5
+  NVIC_EN0_R |=(1<<30);  // (h) enable interrupt 30 in NVIC
+  //EnableInterrupts();           // (i) Program 5.3
+	/*********************end of modification***************************************/
+}
+// add a background task to run whenever the SW1 (PF4) button is pushed
+// Inputs: pointer to a void/void background function
+//         priority 0 is the highest, 5 is the lowest
+// Outputs: 1 if successful, 0 if this thread can not be added
+//Modified by annyan
+void(* SW1_task)(void);
+unsigned long SW1_priority;
+int OS_AddSW1Task(void(*task)(void), unsigned long priority){
+  PortF4_Init();
+	SW1_task = task;
+	SW1_priority = priority;
+	return 1;
+}
+void (* SW2_task)(void);
+unsigned long SW2_priority;
+int OS_AddSW2Task(void(*task)(void), unsigned long priority){
+	PortF0_Init();
+	SW2_task = task;
+	SW2_priority = priority;
+	return 1;
+}
+
+unsigned long OS_Id(void){
+  return RunPt->Id;
+}
+//End of Modification
+/*******************************************Lab2 part2 modified on Feb 15th****************************************/
+//Modified by annyan
+void OS_bSignal(Sema4Type *semaPt){
+  long status;
+	status = StartCritical ();
+	if(semaPt->Value >=1)
+		semaPt->Value = 1;
+	else
+	  semaPt->Value ++;
+	if (semaPt->Value<=0){
+		if (semaPt->first !=NULL){
+			struct Blocked_list_elem * cur = semaPt->first;
+			cur = cur->Next;
+			semaPt->first->Bloker->status = ACTIVE;
+			free (semaPt->first);
+			semaPt->first = cur;
+		}
+	}
+	//using round robin, do not suspend execution
+	EndCritical (status);
+}
+
+void OS_bWait(Sema4Type *semaPt){
+	long status;
+	status = StartCritical ();
+	
+	semaPt->Value--;//Decrement the semaphore counter
+	//UART_OutChar(semaPt->Value+'0');
+	if (semaPt->Value < 0){
+		//UART_OutChar('s');
+		//EnableInterrupts();
+		//DisableInterrupts();
+		if (semaPt->first == NULL){
+			//UART_OutChar('t');
+			semaPt->first = (struct Blocked_list_elem *)malloc (sizeof(struct Blocked_list_elem));
+			semaPt->first->Bloker = RunPt;
+			RunPt->status = BLOCKED;
+			semaPt->first->Next = NULL;
+		}
+		else{
+			struct Blocked_list_elem * cur = semaPt->first;
+		  while (cur->Next!=NULL){
+				cur = cur->Next;
+			}
+		cur->Next = (struct Blocked_list_elem *)malloc (sizeof(struct Blocked_list_elem));
+		cur->Next->Bloker = RunPt;
+		RunPt->status = BLOCKED;
+		cur->Next->Next = NULL;
+	
+		}
+
+		EndCritical (status);
+		OS_Suspend();
+	}
+	
+	EndCritical (status);
+}
+
+void OS_Signal(Sema4Type *semaPt){
+	long status;
+	status = StartCritical();
+	semaPt->Value ++;
+	if (semaPt->Value<=0){
+		if (semaPt->first !=NULL){
+			struct Blocked_list_elem * cur = semaPt->first;
+			cur = cur->Next;
+			semaPt->first->Bloker->status = ACTIVE;
+			free (semaPt->first);
+			semaPt->first = cur;
+		}
+	}
+	//using round robin, do not suspend execution
+	EndCritical (status);
+}
+
+void OS_Wait(Sema4Type *semaPt){
+	long status;
+	status = StartCritical();
+	semaPt->Value--;//Decrement the semaphore counter
+	if (semaPt->Value < 0){
+		//EnableInterrupts();
+		//DisableInterrupts();
+		if (semaPt->first == NULL){
+			semaPt->first = (struct Blocked_list_elem *)malloc (sizeof(struct Blocked_list_elem));
+			semaPt->first->Bloker = RunPt;
+			semaPt->first->Bloker->status = BLOCKED;
+			semaPt->first->Next = NULL;
+		}
+		else{
+			struct Blocked_list_elem * cur = semaPt->first;
+		  while (cur->Next!=NULL){
+				cur = cur->Next;
+			}
+			cur->Next = (struct Blocked_list_elem *)malloc (sizeof(struct Blocked_list_elem));
+			cur->Next->Bloker = RunPt;
+			cur->Next->Bloker->status = BLOCKED;
+			cur->Next->Next = NULL;
+		}
+
+		EndCritical(status);
+		OS_Suspend();
+	}
+	
+	EndCritical(status);
+}
+void OS_InitSemaphore(Sema4Type *semaPt, long value){
+  semaPt->Value = value;
+
+	semaPt->first = NULL; //initialize blocked_list
+	return;
+}
+//End of Modification
+#define NVIC_EN0_INT21          0x00200000  // Interrupt 21 enable
+#define TIMER_ICR_TATOCINT      0x00000001  // GPTM TimerA Time-Out Raw
+int SW1_flag=0;
+int Timer1_count=0;
+/**********************************End of Not Implemented*****************************/
+//change to 2A
+void Timer2A_Init(void){
+	long sr = StartCritical(); 
+  SYSCTL_RCGC1_R |= 0x000e0000; // 0) activate timer2
+  TIMER2_CTL_R &= ~0x00000001;     // 1) disable timer2A during setup
+  TIMER2_CFG_R = 0x00000000;       // 2) configure for 32-bit timer mode
+  TIMER2_TAMR_R = 0x00000012;      // 3) configure for periodic mode, default down-up settings
+  TIMER2_TAILR_R = 80000-1;       // 4) reload value
+  TIMER2_ICR_R = 0x00000001;       // 6) clear timer1A timeout flag
+  TIMER2_IMR_R |= 0x00000001;      // 7) arm timeout interrupt
+  NVIC_PRI5_R = (NVIC_PRI5_R&0x1fffffff)|(0<<29); // 8) priority high or low?
+  NVIC_EN0_R = 0x00800000;     // 9) enable interrupt 23 in NVIC
+  TIMER2_CTL_R |= 0x00000001;      // 10) enable timer2A
+	EndCritical(sr);
+}
+unsigned long OS_MsTime(void){
+  return Timer1_count;
+}
+void OS_ClearMsTime(void){
+	Timer1_count = 0;
+}
+void GPIOPortF_Handler(void){
+	if ((GPIO_PORTF_RIS_R&1)&&(GPIO_PORTF_MIS_R&1)){
+		GPIO_PORTF_ICR_R |=0x1; //clear flag;
+		(*SW2_task)();
+		//UART_OutChar('0');
+	}
+	else if (((GPIO_PORTF_RIS_R>>4)&1)&&((GPIO_PORTF_MIS_R>>4)&1)){
+		GPIO_PORTF_ICR_R |=0x10; //clear flag;
+		//UART_OutChar('4');
+		(*SW1_task)();
+	}
+	//UART_OutChar('b');
+	/*PIND0^=0x01;
+	
+	if (!SW1_flag){
+		OS_ClearMsTime();
+		//UART_OutChar('d');
+		(*SW1_task)();
+	
+	}
+	SW1_flag = 1;
+	if(OS_MsTime() > 20){//maybe longer: annyan
+		OS_ClearMsTime();
+		//UART_OutChar('d');
+		(*SW1_task)();
+
+		}*/
+	
+		//PIND0^=0x01;
+    //PIND0^=0x01;
+	
+}
+
+void Timer2A_Handler(void){
+	 TCB_Type *pt;
+   long status;
+	 TIMER2_ICR_R = TIMER_ICR_TATOCINT;// acknowledge timer1A timeout
+	 status = StartCritical();
+	if ((RunPt !=NULL)&&(RunPt->sleep>0)&&(RunPt->status == ACTIVE))
+		RunPt->sleep--;
+	pt = RunPt->Next;
+	 
+	Timer1_count++;
+	count_timer2++;
+	
+  //UART_OutChar('2');
+	
+
+	 while (pt!= RunPt){
+		 if (!RunPt)
+			 break;
+		 if ((pt->sleep>0)&&(pt->status == ACTIVE))
+			 pt->sleep --;
+		 pt = pt->Next;
+	 }
+	
+	 EndCritical (status);
+}
+//New scheduler
+void SysTick_Handler (void){
+//	long status;
+	unsigned int priority = 7;//initialized as the lowest one
+	
+	TCB_Type *pt;
+	//PIND0 = ~PIND0;
+	//PIND0 = ~PIND0;
+	//status = StartCritical();
+	if ((RunPt->sleep == 0) && (RunPt->status == ACTIVE))
+		priority = RunPt -> priority;
+	pt = RunPt->Next;
+		while (pt!=RunPt){
+			if ((pt->priority < priority)&&(pt->sleep==0)&&(pt->status==ACTIVE)){
+				priority = pt->priority;
+			}
+			pt = pt->Next;
+		}
+		pt = RunPt->Next;
+		while (1){
+			if ((pt->priority == priority)&&(pt->sleep==0)&&(pt->status == ACTIVE)){
+				NextPt = pt;
+				break;
+			}
+			pt = pt->Next;
+		}
+		
+		
+		
+	//EndCritical (status);
+	
+	NVIC_INT_CTRL_R = 0x10000000;//trigger PEND_SV handler
+}
